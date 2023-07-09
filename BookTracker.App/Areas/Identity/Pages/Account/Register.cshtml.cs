8cc07d17076d23b2335a62b6d55c4e1602ee392b
Carlo <carlo.bene@web.de>
2023-07-09 12:47:53 +0200

    Add username field to registration form
    Updated the registration form to include a field for username input, and adjusted the 'SetUserNameAsync' method call to use the input username instead of the email. This change was necessary to extend control over the project's user entity and allow users to register a unique username in addition to their email. This improves user experience and prepares for future features such as user profiles.

--- BookTracker.App/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BookTracker.App/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -111,7 +111,7 @@ namespace BookTracker.App.Areas.Identity.Pages.Account
             {
                 var user = CreateUser();
 
-                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
+                await _userStore.SetUserNameAsync(user, Input.Username, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
