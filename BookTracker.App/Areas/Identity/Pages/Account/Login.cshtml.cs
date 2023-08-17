aa3b5ffc0d9974dde6d553876f606a84c6d98de2
Carlo <carlo.bene@web.de>
2023-08-17 19:52:50 +0200

    Add profile picture and username updates on user's profile page
    Added functionality to let users upload a profile picture and change their usernames. Displayed user's profile picture on the navbar. Also, changed the database path in the appsettings.json file, and made necessary changes in the Index.cshtml and _LoginPartial.cshtml files for user interface. Enforced limit on username changes. This will enhance user identification, allow personalisation and improve user experience. The changes made to database path allow better project management.

--- BookTracker.App/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ BookTracker.App/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -39,11 +39,7 @@ namespace BookTracker.App.Areas.Identity.Pages.Account
         /// </summary>
         [BindProperty]
         public InputModel Input { get; set; }
-
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
+        
         public IList<AuthenticationScheme> ExternalLogins { get; set; }
 
         /// <summary>
--- BookTracker.App/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ BookTracker.App/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -46,31 +46,35 @@ namespace BookTracker.App.Areas.Identity.Pages.Account.Manage
         [BindProperty]
         public InputModel Input { get; set; }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
+        [TempData]
+        public string UserNameChangeLimitMessage { get; set; }
+        
         public class InputModel
         {
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
+            [Display(Name = "Username")] 
+            public string Username { get; set; }
+            
             [Phone]
             [Display(Name = "Phone number")]
             public string PhoneNumber { get; set; }
+            
+            [Display (Name = "ProfilePicture")] 
+            public byte[] ProfilePicture { get; set; }
         }
 
         private async Task LoadAsync(ApplicationUser user)
         {
             var userName = await _userManager.GetUserNameAsync(user);
             var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
-
+            var profilePicture = user.ProfilePicture;  
+            
             Username = userName;
 
             Input = new InputModel
             {
-                PhoneNumber = phoneNumber
+                PhoneNumber = phoneNumber,
+                Username = userName,
+                ProfilePicture = profilePicture
             };
         }
 
@@ -81,6 +85,8 @@ namespace BookTracker.App.Areas.Identity.Pages.Account.Manage
             {
                 return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
             }
+
+            UserNameChangeLimitMessage = $"You can change your username {user.UsernameChangeLimit} more time(s).";
 
             await LoadAsync(user);
             return Page();
@@ -110,7 +116,44 @@ namespace BookTracker.App.Areas.Identity.Pages.Account.Manage
                     return RedirectToPage();
                 }
             }
-
+            
+            if (Request.Form.Files.Count > 0)
+            {
+                IFormFile file = Request.Form.Files.FirstOrDefault();
+                using (var dataStream = new MemoryStream())
+                {
+                    await file.CopyToAsync(dataStream);
+                    user.ProfilePicture = dataStream.ToArray();
+                }
+                await _userManager.UpdateAsync(user);
+            }
+            
+            var userName = await _userManager.GetUserNameAsync(user);
+            if (Input.Username != userName)
+            {
+                if (user.UsernameChangeLimit <= 0)
+                {
+                    StatusMessage = "Username change limit reached";
+                    return RedirectToPage();
+                }
+                
+                var userNameExists = await _userManager.FindByNameAsync(Input.Username);
+                if (userNameExists != null)
+                {
+                    StatusMessage = "User name already taken. Select a different username.";
+                    return RedirectToPage();
+                }
+                
+                var setUserNameResult = await _userManager.SetUserNameAsync(user, Input.Username);
+                if (!setUserNameResult.Succeeded)
+                {
+                    StatusMessage = "Unexpected error when trying to set Username.";
+                    return RedirectToPage();
+                }
+                user.UsernameChangeLimit--;
+                await _userManager.UpdateAsync(user);
+            }
+            
             await _signInManager.RefreshSignInAsync(user);
             StatusMessage = "Your profile has been updated";
             return RedirectToPage();
